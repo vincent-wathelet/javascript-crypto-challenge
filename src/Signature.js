e8601a790db9f354485359d58ad211d9d5556c32
vincent-wathelet <32623140+vincent-wathelet@users.noreply.github.com>
2019-10-02 21:05:30 +0200

    signature oplossing

--- src/Signature.js
+++ src/Signature.js
@@ -1,6 +1,22 @@
 const soduim = require('libsodium-wrappers');
+let keypair = null;
+let loadLibsodium = async () => await soduim.ready;
+
+(async () => {
+    await loadLibsodium();
+    keypair = soduim.crypto_sign_keypair();
+})();
 
 module.exports.verifyingKey = async function verifyingKey()
 {
+    await loadLibsodium();
     
+    return keypair.publicKey;
+
+
+}
+
+module.exports.sign = async function sign(msg)
+{
+    return soduim.crypto_sign(msg,keypair.privateKey);
 }
